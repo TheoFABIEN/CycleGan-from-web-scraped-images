a37288b5a9b66bde9e60727cdd23440b7a09b4c0
TheoFABIEN <fabien.theo@gmail.com>
2024-09-23 22:42:02 +0200

    Added hyperparameters in config.py

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean = [.5, .5, .5], std = [.5, .5, .5])
 ])
+
+# Hyperparameters
+BATCH_SIZE = 8
+LR = 2e-4
+NUM_EPOCHS = 20 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LAMBDA_CYCLE = 10
+LAMBDA_ID = 0
